78a41135b19e71cb72127240be427ebdf78e6849
Pratyoosh <pratyooshanand00.17@gmail.com>
2021-06-06 20:27:43 +0530

    swap.cpp

---
+++ swap.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+
+int main()
+{
+string n,n1;
+getline(cin,n);
+getline(cin,n1);
+n.append(n1);
+cout<<n<<endl;
+
+}
